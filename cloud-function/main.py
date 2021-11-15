519141e8465b192ba80fe3c79989f43a1077d0be
Christos Aniftos <aniftos@gmail.com>
2021-11-15 17:27:57 +0000

    fc

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -18,14 +18,12 @@ def pipeline_trigger(event, context):
     pl = PipelineJob(
         enable_caching=True,
         display_name = "xgb-pipeline",
-        job_id = "run-a3b8252",
+        job_id = "schedule-run-a3b8252",
         pipeline_root="gs://feature-store-mars21/vertex-xgb-ml-ops/a3b8252",
         template_path = "gs://feature-store-mars21/vertex-xgb-ml-ops/a3b8252/pipeline.json",
         project = "feature-store-mars21",
         location = "us-central1",
         parameter_values = {"project_id":"feature-store-mars21","data_path":"gs://mortgage_dataset_files/mortgage-small.csv"})
 
-    status = pl.run(sync=False, service_account="pipelines-cloud-build@feature-store-mars21.iam.gserviceaccount.com")
-    
-    if(pl.has_failed):
-        exit(1)
+    pl.run(sync=False, service_account="pipelines-cloud-build@feature-store-mars21.iam.gserviceaccount.com")
+   
