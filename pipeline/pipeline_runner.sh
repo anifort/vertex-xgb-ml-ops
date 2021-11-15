5b56e31e54baf221a24cf416f91f09f51797d118
Christos Aniftos <aniftos@gmail.com>
2021-11-15 01:17:08 +0000

    fc

--- pipeline/pipeline_runner.sh
+++ pipeline/pipeline_runner.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 python setup.py install
-python run-pipeline.py -uri $1 -l $2 -pp $3 -pn $4 -pid $5 -ruri $6 -rn $7
+python run-pipeline.py -uri $1 -l $2 -pp $3 -pn $4 -pid $5 -ruri $6 -rn $7 -sa $8
